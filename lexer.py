716e47ef5a1d21641c0ccbb107eb510b18e67bc3
Lucas-Nieto-institucional <l.nietom@uniandes.edu.co>
2023-09-02 14:23:52 -0500

    Otro commit

--- lexer.py
+++ lexer.py
@@ -4,18 +4,19 @@ Created on Sat Sep  2 13:59:58 2023
 
 @author: Lucas Nieto Martínez
 @author: 
+
+Lexer
+Módulo de análisis léxico del proyecto 0 del curso ISIS-1106: Lenguaje y Máquinas
 """
 
-import tokens.py
-
-######################
+#----------------------------------------------------
 # TOKENS
-######################
+#----------------------------------------------------
+import tokens
 """
 Token: {Tipo, valor opcional}
 Clase del objeto token:
 """
-
 class token:
     def __init__(self,type_, value):
         self.type = type_
@@ -26,3 +27,8 @@ class token:
         if self.value:
             return f'{self.type}:{self.value}'
         return f'{self.type}'
+    
+#----------------------------------------------------
+# LEXER
+#----------------------------------------------------
+
